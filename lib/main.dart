a1febcca05174cfbc630ad1f6a3b14cf901df3b3
KIMDANWOO <eksdn112@gamil.com>
2025-07-27 10:18:04 +0900

    alingnment 색상 위치 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       home: Scaffold(
         appBar: AppBar( title: Text('앱임')),
-        body: Container(
-          width: 150, height: 50,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black)
+        body: Align(
+          alignment: Alignment.topCenter,
+          child:Container(
+          width: 150, height: 50, color: Colors.blue,
           ),
         ),
       ),
     );
   }
 }
-
+ //double.infinity 를 width나 height 뒤에 숫자대신 넣으면 무한대로 색입혀줌
