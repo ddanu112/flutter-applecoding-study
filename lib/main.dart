f629a448be55c91064dd75d0aa4356214f02c15d
KIMDANWOO <eksdn112@gamil.com>
2025-07-30 08:28:01 +0900

    레이아웃 예시

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( leading: Icon(Icons.star) ,title: Text('ddddd')),
-        body: SizedBox(
-
+        appBar: AppBar(),
+        body: Container(
+          height: 100,
+          child: Row(
+            children: [
+              Image.asset('dog.jpg', width: 200),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('카메라팝니다', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
+                    Text('금호동 3가'),
+                    Text('7000원'),
+                    Row(
+                      children: const [
+                        Icon(Icons.favorite),
+                        Text('4')
+                      ],
+                    ), //Row
+                  ],
+                ), //Column
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-//TextButton(), IconButton(), ElevatedButton()
